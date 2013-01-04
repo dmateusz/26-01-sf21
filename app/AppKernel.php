63722430ceddddd35f8809dbae218326d7ddde8e
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2013-01-04 16:12:31 +0100

    bundle-i-dane-tekstowe

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new My\UserBundle\MyUserBundle(),
+            new My\FrontendBundle\MyFrontendBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
